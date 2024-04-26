728cdb0675e5fb761c522abb6972af371dda42d3
Your Name <your.email@example.com>
2024-04-26 09:18:50 +0200

    "kaput "

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
+document.body.appendChild(renderer.domElement);
 
 camera.position.z = 5;
 
+const planeGeometry = new THREE.PlaneGeometry(5, 5);
+const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x00ffff });
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(planeMesh);
+console.log(planeMesh);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 0, 1);
+scene.add(light);
 function animate() {
-
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
+  requestAnimationFrame(animate);
+  // planeMesh.rotation.x += 0.01;
+  renderer.render(scene, camera);
 }
 
 animate();
-
