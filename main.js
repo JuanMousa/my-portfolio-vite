3cd11cbdbf7572cc322be4bdfd98ce700fb9b270
JaunMousa <juan.mousa@powercoders.org>
2024-05-06 17:43:17 +0200

    the project with GUI in interface

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
 import * as THREE from "three";
+import * as dat from "dat.gui";
+import { mapLinear } from "three/src/math/MathUtils.js";
+
+const gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthAssagmnet: 10,
+    heightAssagment: 10,
+  },
+};
+gui.add(world.plane, "width", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "height", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "widthAssagmnet", 1, 20).onChange(generatePlane);
+gui.add(world.plane, "heightAssagment", 1, 20).onChange(generatePlane);
+
+function generatePlane() {
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width,
+    world.plane.height,
+    world.plane.widthAssagmnet,
+    world.plane.heightAssagment
+  );
+
+  const { array } = planeMesh.geometry.attributes.position;
+
+  for (let i = 0; i < array.length; i++) {
+    const x = array[i];
+    const y = array[i + 1];
+    const z = array[i + 2];
+
+    array[i + 2] = z + Math.random(); // to make it fuzzy
+  }
+}
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -14,11 +50,26 @@ document.body.appendChild(renderer.domElement);
 
 camera.position.z = 5;
 
-const planeGeometry = new THREE.PlaneGeometry(5, 5);
-const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x0000ff });
+const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+const planeMaterial = new THREE.MeshPhongMaterial({
+  color: 0x68d2e8,
+  side: THREE.DoubleSide,
+  flatShading: true,
+});
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
-console.log(planeMesh);
+
+const { array } = planeMesh.geometry.attributes.position;
+
+for (let i = 0; i < array.length; i++) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+
+  array[i + 2] = z + Math.random(); // to make it fuzzy
+
+  // console.log(array[i]);
+}
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 1);
---
+++ readMe.em
@@ -0,0 +1,8 @@
+the planMeshArr deicliration is {
+    buffer: ArrayBuffer(48): This indicates that the Float32Array is backed by an ArrayBuffer object of 48 bytes. ArrayBuffers are used to store raw binary data.
+    byteLength: 48: This confirms the size of the ArrayBuffer in bytes.
+    byteOffset: 0: This specifies that the Float32Array starts at the beginning of the ArrayBuffer.
+    length: 12: This reiterates that the Float32Array has a length of 12 elements.
+    Symbol(Symbol.toStringTag): 'Float32Array': This is an internal symbol used by JavaScript to identify the object as a Float32Array.
+}  and you have to use the array till the last 0 so its [-2.5, 2.5,0] and its use to define the Vertices
+    x, y, z
